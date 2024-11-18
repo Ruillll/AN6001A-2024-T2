975a4ef6c4d0733aa14a95c611b032f87c7b84dc
Ruillll <rluan001@e.ntu.edu.sg>
2024-11-18 12:10:31 +0000

    v1 11.18

---
+++ Homework1.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("q&a.html"))
+
+
+if __name__ == "__main__":
+    app.run()
